91b50a7d81a07484d60f80f649bc08a8bf6e399a
DavidPavach <davidpavach@proton.me>
2026-03-17 14:57:02 +0100

    fix: final header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -129,12 +129,12 @@ const Header = () => {
 
     return (
         <header className="relative flex justify-between items-center bg-background px-2 py-3 border-border border-b">
-            <a href="/profile#kyc" className="group inline-block relative">
+            <a href="/profile#kyc" className="flex items-baseline gap-x-2">
                 <Avatar className="border border-border rounded-full">
                     <AvatarImage src={profilePicture || "/user.svg"} alt="default profile" />
                     <AvatarFallback>TL</AvatarFallback>
                 </Avatar>
-                <div className="right-0 -bottom-1 absolute flex items-center gap-x-1">
+                <div className="flex items-center gap-x-1">
                     <span className={`size-3 rounded-full ${status === "pending" ? "bg-amber-500" : status === "accepted" ? "bg-green-500" : "bg-destructive"}`} />
                     <span className={`px-2 py-1 border border-border rounded-md ${status === "pending" ? "text-amber-500" : status === "accepted" ? "text-green-500" : "text-destructive"} text-[8px] md:text-[9px] xl:text-[10px] first-letter:uppercase whitespace-nowrap`}>
                         {status}
